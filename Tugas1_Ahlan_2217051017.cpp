da56605fa8c1c2482a6c72f8ac05586974df0d5a
Ahlan Sayyid Alghiffari <112612196+AhlanAlghiff@users.noreply.github.com>
2022-09-08 13:15:03 +0700

    Add files via upload

---
+++ Tugas1_Ahlan_2217051017.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//ahlansayyidalghiffari_2217051017
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << a + b<< endl;
+	cout << a - b<< endl;
+	cout << a * b<< endl;
+	cout << a / b<< endl;
+	cout << a % b<< endl;
+    return 0;
+}
