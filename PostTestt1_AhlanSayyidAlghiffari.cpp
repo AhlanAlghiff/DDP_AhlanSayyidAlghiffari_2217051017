6e1581b3507fc8704ab8d34132ebc770d5ba0d58
Ahlan Sayyid Alghiffari <112612196+AhlanAlghiff@users.noreply.github.com>
2022-09-09 11:18:06 +0700

    Add files via upload

---
+++ PostTestt1_AhlanSayyidAlghiffari.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+//ahlansayyidalghiffari_2217051017
+int main()
+{
+	int a;
+	float b;
+	string c;
+	char d;
+	
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	
+	cout << "Integer :"<< a << endl;
+	cout << "Floating-Point :" << b << endl;
+	cout << "String :"<< c << endl;
+	cout << "Character :"<< d << endl;
+}
