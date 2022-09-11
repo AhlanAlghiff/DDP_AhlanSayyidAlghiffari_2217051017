f7b802a32d69019f0bf272f9920f8621a6edcd80
Ahlan Sayyid Alghiffari <112612196+AhlanAlghiff@users.noreply.github.com>
2022-09-11 09:32:19 +0700

    Add files via upload

---
+++ 2217051017_AhlanSayyidAlghiffari_PostTest1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+/*
+Nama : ahlan sayyid alghiffari
+NPM  : 2217051017
+*/
+
+using namespace std;
+
+int main()
+{
+	int a;
+	float b;
+	string c;
+	char d;
+	
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	
+	cout << endl << endl;
+	
+	cout << "Tipe Data :" << endl;
+	
+	cout << "Integer \t: "<< a << endl;
+	cout << "Floating-Point \t: " << b << endl;
+	cout << "String \t\t: "<< c << endl;
+	cout << "Character \t: "<< d << endl;
+	
+	return 0;
+}
